6858c1d886d954c16453fcf55e24720cd8b0df2a
midCenturyDude7 <mgriffes@gmail.com>
2023-03-11 09:40:59 -0800

    Finished basic scaffolding and set up Sass for styling

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Main() {
-  return <div className="main">[home page]</div>;
+  return (
+    <div className="main">
+      <h1 className="main-title">[home page]</h1>
+    </div>
+  );
 }
 
 export default Main;
