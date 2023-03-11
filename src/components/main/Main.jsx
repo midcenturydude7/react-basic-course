164f07f7ee49aa9234e878e6560c8a18e15edbfe
midCenturyDude7 <mgriffes@gmail.com>
2023-03-11 10:34:27 -0800

    Added decrement functionality

--- src/components/main/Main.jsx
+++ src/components/main/Main.jsx
@@ -3,15 +3,26 @@ import React from "react";
 function Main() {
   const [count, setCount] = React.useState(0);
 
-  function handleChange() {
+  function handleIncrement() {
     setCount(count + 1);
+  }
+
+  function handleDecrement() {
+    if (count > 0) {
+      setCount(count - 1);
+    }
   }
   return (
     <div className="main">
       <h1 className="main-title">Total: {count} </h1>
-      <button className="main-button" onClick={handleChange}>
-        Counter
-      </button>
+      <div className="button-container">
+        <button className="main-button" onClick={handleIncrement}>
+          +
+        </button>
+        <button className="main-button" onClick={handleDecrement}>
+          -
+        </button>
+      </div>
     </div>
   );
 }
