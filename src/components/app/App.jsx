07bdae8c4b66e86602b7ef94ea8fc214a14eab9f
midCenturyDude7 <mgriffes@gmail.com>
2023-03-13 15:27:35 -0700

    Setting up routes and linking w/in app

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Header from "../header/Header";
 import Main from "../main/Main";
 import Footer from "../footer/Footer";
 
 function App() {
   return (
-    <div className="layout">
+    <div className="app-layout">
       <Header />
       <Main />
       <Footer />
+      {/* <Routes>
+        <Route path="/" element={<Counter />} />
+        <Route pat="/todo-list" element={<TodoList />} />
+      </Routes> */}
     </div>
   );
 }
